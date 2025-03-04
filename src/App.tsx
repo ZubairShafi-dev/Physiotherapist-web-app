d0fe4460b52d2fb545b763e0fb4fb9bebb82b9dd
Samman Qaiser <sammansheikh6@gmail.com>
2025-03-04 12:38:07 +0500

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Dashboardroute from "./features/Dashboard/routes/Dashboardroute"
 import './index.css'
 import { Therapistroute } from "./features/Therapists/routes/Therapistroute"
 import Bilingroutes from "./features/Biling/routes/Bilingroutes"
-import Appointmentroutes from "./features/Appointments/routes/appointmentroutes"
+import Appointmentroutes from "./features/Appointments/routes/Appointmentroutes"
 import Reportroutes from "./features/Reports/routes/Reportsroutes"
 import Notificationroute from "./features/Notification/routes/Notificationroute"
 import Messageroutes from "./features/Messages/routes/Messageroutes"
